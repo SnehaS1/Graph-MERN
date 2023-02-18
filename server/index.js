cef4420878cc9e0796843b47ab46c8fafe0b1a89
Sneha S <snehasushatha@gmail.com>
2023-02-18 12:30:48 +0400

    db setup

--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
 const app = express();
+
+connectDB();
 
 app.use('/graphql', graphqlHTTP({
   schema,
